e388518fe37dd0726fb74d41a3b89af2cd7afa19
Hardeep Singh <hsingh@servicetitan.com>
2020-09-15 15:24:40 -0700

    Build System

--- app/src/main/java/com/servicetitan/android/platform/android/hiltpoc/ViewModel.kt
+++ app/src/main/java/com/servicetitan/android/platform/android/hiltpoc/ViewModel.kt
@@ -3,7 +3,7 @@ package com.servicetitan.android.platform.android.hiltpoc
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 
-class MainViewModel @ViewModelInject constructor(val someInt: Int): ViewModel() {
+class MainViewModel @ViewModelInject constructor(private val someInt: Int): ViewModel() {
 
     fun sendAppName() = "appNameTest"
 
